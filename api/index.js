9f2dcf12438144a850092c3a54b442d27dca0e20
Matheus Gusman <matheusenrik@hotmail.com>
2020-03-28 23:24:01 -0300

    melhorando organizacao

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const fs = require('fs')
 const morgan = require('morgan')
- const bodyParser = require('body-parser')
+const bodyParser = require('body-parser')
 const app = express();
 app.use(bodyParser.json());
 app.use(express.json());
